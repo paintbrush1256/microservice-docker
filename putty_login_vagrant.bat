20e5d58ea5f3fe77a1d3cd1acf426ca609b98acb
Shibu TN <tnshibu@yahoo.com>
2020-09-03 02:12:02 +0530

    a

--- putty_login_vagrant.bat
+++
@@ -1,5 +0,0 @@
-rem start D:\Programs_Portable_GIT\putty_32bit\putty.exe -load "vagrant" -l vagrant -i D:\vagrant\ubuntu_xenial64\microservice-docker\.vagrant\machines\default\virtualbox\private_key.ppk
-
-
-start D:\Programs_Portable_GIT\kitty\kitty.exe -P 2400 -load "vagrant" -l vagrant -i .\.vagrant\machines\default\virtualbox\private_key.ppk
-
